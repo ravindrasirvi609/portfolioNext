ff3e1c299263c280954516eb8bdbafa18ae15682
Ravindra <sirviravindra609@gmail.com>
2023-12-22 11:42:59 +0530

    Update Tailwind configuration and remove unused files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
-const SkillCard = ({ title, items }: { title: string; items: string[] }) => (
+const SkillCard = ({ title, items }: { title: string, items: string[] }) => (
   <div className="p-4 border rounded-md shadow-md bg-sky-800">
     <h3 className="text-xl font-bold mb-2">{title}</h3>
     <ul className="list-disc list-inside">
@@ -12,13 +12,7 @@ const SkillCard = ({ title, items }: { title: string; items: string[] }) => (
   </div>
 );
 
-const ProjectCard = ({
-  title,
-  description,
-}: {
-  title: string;
-  description: string;
-}) => (
+const ProjectCard = ({ title, description }: { title: string, description: string }) => (
   <div className="p-4 border rounded-md shadow-md bg-sky-800">
     <h3 className="text-xl font-bold mb-2">{title}</h3>
     <p>{description}</p>
@@ -37,28 +31,20 @@ export default function Home() {
     },
     {
       title: "Other Skills",
-      items: [
-        "Version Control (Git)",
-        "API Design and Integration",
-        "UI/UX Design",
-      ],
+      items: ["Version Control (Git)", "API Design and Integration", "UI/UX Design"],
     },
   ];
 
   const projects = [
-    {
-      title: "Operant Next Project",
-      description: "Pharma students and conference management system.",
-    },
-    {
-      title: "Project 2",
-      description: "Description of the project goes here.",
-    },
+    { title: "Project 1", description: "Description of the project goes here." },
+    { title: "Project 2", description: "Description of the project goes here." },
+    // Add more projects as needed
   ];
 
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen p-8 md:p-24 bg-gradient-to-br from-sky-100 via-gray-200 to-sky-300">
-      <div className="relative flex items-center justify-center w-full h-64 md:h-96 before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]"></div>
+    <main className="flex flex-col items-center justify-center min-h-screen p-8 md:p-24 bg-gradient-to-br from-white via-gray-100 to-gray-200">
+      <div className="relative flex items-center justify-center w-full h-64 md:h-96 before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      </div>
       <div className="text-center">
         <h1 className="font-bold text-4xl md:text-6xl mb-4 text-sky-800">
           Welcome! I'm Ravindra,
@@ -107,10 +93,7 @@ export default function Home() {
         <h2 className="text-3xl font-bold text-sky-800 mb-4">Contact</h2>
         <form className="max-w-md mx-auto">
           <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="name"
-            >
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
               Name
             </label>
             <input
@@ -121,10 +104,7 @@ export default function Home() {
             />
           </div>
           <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="email"
-            >
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
               Email
             </label>
             <input
@@ -135,10 +115,7 @@ export default function Home() {
             />
           </div>
           <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="message"
-            >
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="message">
               Message
             </label>
             <textarea
@@ -151,6 +128,7 @@ export default function Home() {
           <button
             className="btn bg-red-400 rounded-3xl text-sky-800 font-bold py-2 px-4 hover:bg-red-800"
             type="button"
+            
           >
             Send Message
           </button>
