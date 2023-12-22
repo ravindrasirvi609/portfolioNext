c5f324e17708ff2c770bacd8c98703ae3240d355
Ravindra <sirviravindra609@gmail.com>
2023-12-22 13:06:04 +0530

    Remove unused import and comment out component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const SkillCard = ({ title, items }: { title: string; items: string[] }) => (
   <div className="p-4 border rounded-md shadow-md bg-sky-500">
@@ -81,12 +80,11 @@ export default function Home() {
 
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-8 md:p-24 bg-gradient-to-br from-white via-gray-100 to-gray-200">
-      <div className="relative flex items-center justify-center w-full h-64 md:h-96 before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]"></div>
+      {/* Hero Section */} 
       <div className="text-center">
         <h1 className="font-bold text-4xl md:text-6xl mb-4 text-sky-800">
           Welcome! I'm Ravindra,
         </h1>
-        <SpeedInsights></SpeedInsights>
         <p className="text-lg md:text-xl text-gray-700">
           A seasoned full-stack developer passionate about creating digital
           solutions that make an impact. Let's embark on a coding adventure!
