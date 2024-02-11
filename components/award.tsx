28d086e2ed6aa074cc27f032ffd77075edb13bcf
Ravindra <sirviravindra609@gmail.com>
2024-02-11 21:50:59 +0530

    Layout Formating

--- components/award.tsx
+++ components/award.tsx
@@ -6,7 +6,7 @@ interface AwardProps {
 
 const Awards: React.FC<AwardProps> = () => {
   return (
-      <section className="mt-12">
+    <section className="mt-12">
       <h2 className="text-3xl font-bold text-sky-200 mb-4">Awards</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         <div className="p-4 border rounded-md shadow-md bg-sky-500">
