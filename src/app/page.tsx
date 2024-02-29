ad8612354b432e6a69ff6bd1fe133b5dfa608f48
Ravindra <sirviravindra609@gmail.com>
2024-02-29 08:08:44 +0530

    Refactor contact form styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -139,12 +139,12 @@ export default function Home() {
       <section className="container mx-auto p-4 md:py-12 rounded-lg shadow-md mt-16 ">
         <div className="w-full dark:bg-black bg-white dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex flex-col items-center justify-center">
           {/* Radial gradient for the container to give a faded look */}
-          <div className="absolute inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+          <div className="absolute inset-0 z-0 dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
           <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
             Contact
           </p>
 
-          <form className="w-full max-w-md px-4">
+          <form className="w-full max-w-md px-4 relative z-10">
             <div className="mb-4">
               <label
                 htmlFor="name"
@@ -192,7 +192,7 @@ export default function Home() {
 
             <button
               type="button"
-              className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              className="w-full mb-16 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             >
               Send Message
             </button>
