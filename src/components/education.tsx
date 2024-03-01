27c2daeb970a01e80560fc9bcbbda4936e962a5d
Ravindra <sirviravindra609@gmail.com>
2024-03-01 22:24:02 +0530

    Add BackgroundGradient component to Education component

--- src/components/education.tsx
+++ src/components/education.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BackgroundGradient } from "./ui/background-gradient";
 
 interface EducationProps {
   // Define your props here
@@ -10,22 +11,27 @@ const Education: React.FC<EducationProps> = () => {
       <section className="mt-12">
         <h2 className="text-3xl font-bold text-sky-200 mb-4">Education</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-          <div className="p-4 border rounded-md shadow-md bg-sky-500">
-            <h3 className="text-xl font-bold text-black mb-2">
+          <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900">
+            <h3 className="text-2xl font-bold text-black dark:text-white">
               Bachelor of Technology in Computer Science and Engineering
             </h3>
-            <p className="text-gray-700 mb-2">2019 - 2023</p>
-            <p className="text-gray-700">
+            <p className="text-gray-700 dark:text-neutral-300 mb-2">
+              2019 - 2023
+            </p>
+            <p className="text-gray-700 dark:text-neutral-300">
               <a href="https://paruluniversity.ac.in/" className="underline">
                 Parul University
               </a>{" "}
               | Vadodara, India
             </p>
-          </div>
-          <div className="p-4 border rounded-md shadow-md bg-sky-500">
-            <h3 className="text-xl font-bold text-black mb-2">Higher Secondary School</h3>
-            <p className="text-gray-700 mb-2">2018</p>
-            <p className="text-gray-700">
+          </BackgroundGradient>
+
+          <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900">
+            <h3 className="text-2xl font-bold text-black dark:text-white mb-2">
+              Higher Secondary School
+            </h3>
+            <p className="text-gray-700 dark:text-neutral-300 mb-2">2018</p>
+            <p className="text-gray-700 dark:text-neutral-300">
               <a
                 href="https://rajeduboard.rajasthan.gov.in/"
                 className="underline"
@@ -34,7 +40,7 @@ const Education: React.FC<EducationProps> = () => {
               </a>{" "}
               | Jodhpur, India
             </p>
-          </div>
+          </BackgroundGradient>
         </div>
       </section>
     </div>
