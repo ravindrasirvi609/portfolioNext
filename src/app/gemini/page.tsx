3263b45b74cbb8f326a64046c1ba7e6884ac2307
Ravindra <sirviravindra609@gmail.com>
2024-05-16 18:02:00 +0530

    refactor: Update page.tsx to improve chatbot section styling and API endpoint

--- src/app/gemini/page.tsx
+++ src/app/gemini/page.tsx
@@ -6,7 +6,7 @@ const Gemini = () => {
   return (
     <div className="bg-black p-4 w-full">
       <h1 className="text-2xl text-yellow-400 sm:text-3xl md:text-4xl lg:text-5xl font-bold text-center flex justify-center items-center">
-        Test Chat-Bot
+        AI Assistant by Ravindra{" "}
       </h1>
       <div className="w-full mx-auto">
         <OpenAi />
