8e63256ad32ea46967277ed52a68809c59fc9f05
Ravindra <sirviravindra609@gmail.com>
2024-05-16 18:00:24 +0530

    refactor: Update page.tsx to improve chatbot section styling and API endpoint

--- src/app/gemini/page.tsx
+++ src/app/gemini/page.tsx
@@ -5,10 +5,10 @@ import React from "react";
 const Gemini = () => {
   return (
     <div className="bg-black p-4 w-full">
-      <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-center flex justify-center items-center">
-        Test this Chat-Bot
+      <h1 className="text-2xl text-yellow-400 sm:text-3xl md:text-4xl lg:text-5xl font-bold text-center flex justify-center items-center">
+        Test Chat-Bot
       </h1>
-      <div className="w-full sm:w-3/4 md:w-1/2 lg:w-1/3 mx-auto">
+      <div className="w-full mx-auto">
         <OpenAi />
       </div>
     </div>
