f8967b3683981865b8845d2e3d96dedaa45d5a43
Ravindra <sirviravindra609@gmail.com>
2024-05-22 15:48:08 +0530

    refactor: Update userData route to use environment variable for Resend API key

--- src/app/api/userData/route.ts
+++ src/app/api/userData/route.ts
@@ -21,60 +21,90 @@ export async function POST(req: NextRequest) {
     }
     const resend = new Resend(apiKey);
     console.log(resend);
-
-    resend.emails.send({
-      from: "dev@ravindrachoudhary.in",
-      to: "ravi.sirvi609@gmail.com",
-      subject: `${newUser.name} wants to connect with you!`,
-      html: `
-    <div style="font-family: Arial, sans-serif; padding: 20px; color: #333;">
-      <h1 style="font-size: 24px; color: #333;">Connection Request</h1>
-      <p style="font-size: 18px;">Hi,</p>
-      <p style="font-size: 18px;"><strong>${newUser.name}</strong> wants to connect with you!</p>
-      <p style="font-size: 18px;">Here are their details:</p>
-      <ul style="font-size: 18px;">
-        <li>Email: ${newUser.email}</li>
-        <li>Message: ${newUser.message}</li>
-        <li>Mobile No: ${newUser.mobileNo}</li>
-        <li>Created At: ${newUser.createdAt}</li>
-      </ul>
-    </div>
-  `,
+    const res = await fetch("https://api.resend.com/emails", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${apiKey}`,
+      },
+      body: JSON.stringify({
+        from: "dev@ravindrachoudhary.in",
+        to: ["ravi.sirvi609@gmail.com"],
+        subject: "hello world",
+        html: `<div style="font-family: Arial, sans-serif; padding: 20px; color: #333;">
+        <h1 style="font-size: 24px; color: #333;">Connection Request</h1>
+         <p style="font-size: 18px;">Hi,</p>
+         <p style="font-size: 18px;"><strong>${newUser.name}</strong> wants to connect with you!</p>
+         <p style="font-size: 18px;">Here are their details:</p>
+         <ul style="font-size: 18px;">
+           <li>Email: ${newUser.email}</li>
+           <li>Message: ${newUser.message}</li>
+           <li>Mobile No: ${newUser.mobileNo}</li>
+           <li>Created At: ${newUser.createdAt}</li>
+         </ul>
+       </div>`,
+      }),
     });
 
-    resend.emails.send({
-      from: "dev@ravindrachoudhary.in",
-      to: `${newUser.email}`,
-      subject: `Thanks for Connecting, ${newUser.name}`,
-      html: `
-    <div style="font-family: Arial, sans-serif; padding: 20px; color: #333; max-width: 600px; margin: auto; border: 1px solid #ddd; border-radius: 8px;">
-      <h1 style="font-size: 24px; color: #333; text-align: center; margin-bottom: 20px;">Connection Request</h1>
-      <p style="font-size: 18px;">Hi ${newUser.name},</p>
-      <p style="font-size: 18px;">
-        Thank you for reaching out! I have received your request and will get back to you soon.
-      </p>
-      <p style="font-size: 18px;">
-        If you need to contact me directly, please feel free to call me at <strong>8107199052</strong>.
-      </p>
-      <p style="font-size: 18px;">Best regards,</p>
-      <p style="font-size: 18px; font-weight: bold;">Ravindra Choudhary</p>
-    </div>
-    <style>
-      @media only screen and (max-width: 600px) {
-        div {
-          padding: 15px;
-          font-size: 16px;
-        }
-        h1 {
-          font-size: 22px;
-        }
-        p {
-          font-size: 16px;
-        }
-      }
-    </style>
-  `,
-    });
+    if (res.ok) {
+      const data = await res.json();
+      console.log(data);
+    } else {
+      console.error("Error sending email:", res.status);
+    }
+    //   resend.emails.send({
+    //     from: "dev@ravindrachoudhary.in",
+    //     to: "ravi.sirvi609@gmail.com",
+    //     subject: `${newUser.name} wants to connect with you!`,
+    //     html: `
+    //   <div style="font-family: Arial, sans-serif; padding: 20px; color: #333;">
+    //     <h1 style="font-size: 24px; color: #333;">Connection Request</h1>
+    //     <p style="font-size: 18px;">Hi,</p>
+    //     <p style="font-size: 18px;"><strong>${newUser.name}</strong> wants to connect with you!</p>
+    //     <p style="font-size: 18px;">Here are their details:</p>
+    //     <ul style="font-size: 18px;">
+    //       <li>Email: ${newUser.email}</li>
+    //       <li>Message: ${newUser.message}</li>
+    //       <li>Mobile No: ${newUser.mobileNo}</li>
+    //       <li>Created At: ${newUser.createdAt}</li>
+    //     </ul>
+    //   </div>
+    // `,
+    //   });
+
+    //   resend.emails.send({
+    //     from: "dev@ravindrachoudhary.in",
+    //     to: `${newUser.email}`,
+    //     subject: `Thanks for Connecting, ${newUser.name}`,
+    //     html: `
+    //   <div style="font-family: Arial, sans-serif; padding: 20px; color: #333; max-width: 600px; margin: auto; border: 1px solid #ddd; border-radius: 8px;">
+    //     <h1 style="font-size: 24px; color: #333; text-align: center; margin-bottom: 20px;">Connection Request</h1>
+    //     <p style="font-size: 18px;">Hi ${newUser.name},</p>
+    //     <p style="font-size: 18px;">
+    //       Thank you for reaching out! I have received your request and will get back to you soon.
+    //     </p>
+    //     <p style="font-size: 18px;">
+    //       If you need to contact me directly, please feel free to call me at <strong>8107199052</strong>.
+    //     </p>
+    //     <p style="font-size: 18px;">Best regards,</p>
+    //     <p style="font-size: 18px; font-weight: bold;">Ravindra Choudhary</p>
+    //   </div>
+    //   <style>
+    //     @media only screen and (max-width: 600px) {
+    //       div {
+    //         padding: 15px;
+    //         font-size: 16px;
+    //       }
+    //       h1 {
+    //         font-size: 22px;
+    //       }
+    //       p {
+    //         font-size: 16px;
+    //       }
+    //     }
+    //   </style>
+    // `,
+    //   });
 
     return NextResponse.json({
       message: "User added successfully",
