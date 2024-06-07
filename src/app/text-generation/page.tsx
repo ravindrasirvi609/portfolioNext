6667a1d9d9909801b81da0c116a6312f303c68c8
Ravindra <sirviravindra609@gmail.com>
2024-06-07 16:13:31 +0530

    refactor: Update text-generation page with AI Assistant functionality and UI improvements

--- src/app/text-generation/page.tsx
+++ src/app/text-generation/page.tsx
@@ -1,15 +1,56 @@
 "use client";
-import OpenAi from "@/components/openAi";
+import { PlaceholdersAndVanishInput } from "@/components/placeholders-and-vanish-input";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 const Gemini = () => {
+  const placeholders = [
+    "What's the first rule of Fight Club?",
+    "Who is Tyler Durden?",
+    "Where is Andrew Laeddis Hiding?",
+    "Write a Javascript method to reverse a string",
+    "How to assemble your own PC?",
+  ];
+
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+  const [isLoading, setIsLoading] = useState(false); // State for loader visibility
+  const [error, setError] = useState(""); // State for error message
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+    setInput(e.target.value);
+  };
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsLoading(true); // Set loading to true when starting the request
+    try {
+      const response = await fetch("/api/chatBot", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ input }),
+      });
+
+      if (!response.ok) {
+        throw new Error("An error occurred while fetching data");
+      }
+
+      const data = await response.json();
+      setOutput(data.text);
+    } catch (error: any) {
+      setError(error.message);
+    } finally {
+      setIsLoading(false); // Set loading to false when the request completes
+    }
+    console.log("submitted");
+  };
+
   return (
     <div className="bg-black p-4 w-full">
       <div className="flex justify-between items-center">
-        <h1 className="text-2xl text-yellow-400 sm:text-3xl md:text-4xl lg:text-5xl font-bold">
-          AI Assistant by Ravindra
-        </h1>
         <Link
           href={"/"}
           className="bg-blue-500 text-black rounded-lg p-2 hover:bg-blue-700"
@@ -17,8 +58,28 @@ const Gemini = () => {
           Back To Profile
         </Link>
       </div>
-      <div className="w-full mx-auto">
-        <OpenAi />
+
+      <div className="h-[40rem] flex flex-col justify-center items-center px-4">
+        <h2 className="mb-10 sm:mb-20 text-xl text-center sm:text-5xl text-yellow-400">
+          Ask AI Assistant Anything
+        </h2>
+        <PlaceholdersAndVanishInput
+          placeholders={placeholders}
+          onChange={handleChange}
+          onSubmit={onSubmit}
+          isLoading={isLoading}
+        />
+        {error && (
+          <div>
+            <p className="text-red-500 bg-yellow-400">{error}</p>
+          </div>
+        )}
+        <div
+          className="mt-8 text-white w-full text-center"
+          style={{ whiteSpace: "pre-line" }}
+        >
+          {output}
+        </div>
       </div>
     </div>
   );
